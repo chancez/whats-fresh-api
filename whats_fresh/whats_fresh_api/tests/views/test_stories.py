6a4392d18a2fd5c7bff0208c2f2ec7b1fd98be53
MaraJade <catbookgirl@gmail.com>
2014-09-30 12:06:57 -0700

    Updated tests to reflect the updated story model
    refs #17091

--- whats_fresh/whats_fresh_api/tests/views/test_stories.py
+++ whats_fresh/whats_fresh_api/tests/views/test_stories.py
@@ -10,6 +10,8 @@ class StoriesTestCase(TestCase):
     fixtures = ['test_fixtures']
 
     def setUp(self):
+        self.maxDiff = None
+
         self.expected_json = """
 {
     "error": {
@@ -19,7 +21,16 @@ class StoriesTestCase(TestCase):
         "debug": null,
         "level": null
     },
-    "story": "These are the voyages of the Starfish Enterblub; her five year mission -- to seek out new fish and new fishilizations..."
+    "id": 1,
+    "name": "Star Wars",
+    "history": "A long time ago, in a galaxy far, far away...",
+    "facts": "Star Wars is awesome",
+    "buying": "I have no idea what this field is for.",
+    "preparing": "Fried",
+    "products": "Fish",
+    "season": "Spring",
+    "created": "2014-08-08 23:27:05.568395+00:00",
+    "modified": "2014-08-08 23:27:05.568395+00:00"
 }"""
 
         self.expected_not_found = """
@@ -45,6 +56,14 @@ class StoriesTestCase(TestCase):
         expected_answer = json.loads(self.expected_json)
         self.assertEqual(parsed_answer, expected_answer)
 
+    def test_story_items(self):
+        response = self.client.get(
+            reverse('story-details', kwargs={'id': '1'})).content
+        parsed_answer = json.loads(response)
+        expected_answer = json.loads(self.expected_json)
+
+        self.assertEqual(parsed_answer, expected_answer)
+
     def test_story_not_found(self):
         response = self.client.get(
             reverse('story-details', kwargs={'id': '999'}))
@@ -52,5 +71,4 @@ class StoriesTestCase(TestCase):
         self.assertEqual(response.status_code, 404)
 
         expected_answer = json.loads(self.expected_not_found)
-        self.maxDiff = None
         self.assertEqual(parsed_answer, expected_answer)
